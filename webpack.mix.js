23014e1463748f2d48f002e3f12531647f7340cd
valvicthe <louie12brett@icloud.com>
2026-07-19 19:39:18 +0100

    Refactor mix.js and postCss chaining

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,4 @@
 const mix = require('laravel-mix');
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -10,10 +9,7 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js/game.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
+mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
