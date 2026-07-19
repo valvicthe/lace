9c56ff5a32547c9671fd9739c94082ffa675cc46
valvicthe <louie12brett@icloud.com>
2026-07-19 07:48:31 +0100

    Implement site alert bar in navigation layout
    Added an alert bar to display site alerts if set.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -98,8 +98,6 @@
                 </x-dropdown>
             </div>
 
-            
-
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
@@ -207,6 +205,13 @@
         </div>
     </div>
 </nav>
+
+<!-- Alert Bar -->
+@if(isset($siteAlert))
+    <div class="bg-orange-500 text-white text-center py-2 font-bold text-sm shadow-sm">
+        {{ $siteAlert->message }}
+    </div>
+@endif
 
 </head>
 </html>
