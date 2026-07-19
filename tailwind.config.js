a1183f00e612b3bbf4c3a95e2b1a38e9162c2421
valvicthe <louie12brett@icloud.com>
2026-07-19 10:35:08 +0100

    Enable dark mode support in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+    // Add this line to enable dark mode support
+    darkMode: 'class',
+
     purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
