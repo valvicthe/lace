99a30cce2a5079150c4b218efb394bca237fcbf6
valvicthe <louie12brett@icloud.com>
2026-07-19 12:07:38 +0100

    Add use statement for Illuminate Http Request

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
