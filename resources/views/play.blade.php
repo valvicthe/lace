c08c3119d142a651238daa811a1dcd1240e28a00
valvicthe <louie12brett@icloud.com>
2026-07-19 19:14:54 +0100

    Add initial play.blade.php view for game interface

---
+++ resources/views/play.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Raiinv2 Client - Test</title>
+    @vite(['resources/css/app.css', 'resources/js/game.js'])
+    <style>
+        body { margin: 0; overflow: hidden; }
+        canvas { display: block; }
+    </style>
+</head>
+<body>
+    <div id="game-container"></div>
+</body>
+</html>
